f1a41859458ebc10039d64f6a64d005c36f34474
Ksenia Melnyk <melnyksenia@gmail.com>
2024-08-30 08:48:00 +0200

    hw fixed

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 function getElementWidth(content, padding, border) {
-    content = Number.parseFloat(content)
-    padding = Number.parseFloat(padding)
-    border = Number.parseFloat(border)
-    return elementWidth = (content + (padding * 2) + (border * 2))
+    const contentWidth = Number.parseFloat(content);
+    const paddingWidth = Number.parseFloat(padding)
+    const borderWidth = Number.parseFloat(border)
+    return elementWidth = (contentWidth + (paddingWidth * 2) + (borderWidth * 2))
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
